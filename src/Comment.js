96e229c559984cb2274c998377504361708f91ac
codertinie <martin.munyao@moringaschool.com>
2022-09-21 19:54:37 +0300

    Completed assignment

--- src/Comment.js
+++ src/Comment.js
@@ -1,7 +1,12 @@
 import React from "react";
+//using arrow function
+// const Comment = () => <div>Naturally, I agree with this article</div>;
 
-function Comment() {
-  return <div>Naturally, I agree with this article.</div>;
+//class Component
+class Comment extends React.Component {
+  render() {
+    return <div>Naturally, I agree with this article.</div>;
+  }
 }
 
 export default Comment;
